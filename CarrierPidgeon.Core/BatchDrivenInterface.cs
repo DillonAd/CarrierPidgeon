dfe0534cebf4867fa2af9d3d8ea35ae28596d607
Dillon Adams <dillon-a@hotmail.com>
2018-06-05 21:49:49 -0500

    renaming contacts to core

---
+++ CarrierPidgeon.Core/BatchDrivenInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarrierPidgeon.Core
+{
+    public abstract class BatchDrivenInterface
+    {
+        protected int IntervalMilliseconds { get; }
+
+        protected BatchDrivenInterface(int intervalMillisonds)
+        {
+            IntervalMilliseconds = intervalMillisonds;
+        }
+
+        public abstract void Execute();
+    }
+}
---
+++ CarrierPidgeon.Core/EventDrivenInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CarrierPidgeon.Core
+{
+    public abstract class EventDrivenInterface<T>
+    {
+        protected delegate EventHandler OnMessageReceived<T>(T t);
+        protected event OnMessageReceived<T> MessageReceived;
+    }
+}
