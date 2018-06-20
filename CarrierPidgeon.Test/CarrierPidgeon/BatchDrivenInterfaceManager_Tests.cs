bc58601681a50445a8c5eb3e75b373a972f7f7a5
Dillon Adams <dillon-a@hotmail.com>
2018-06-20 13:12:54 -0500

    adding negative test for executing batched interfaces

--- CarrierPidgeon.Test/CarrierPidgeon/BatchDrivenInterfaceManager_Tests.cs
+++ CarrierPidgeon.Test/CarrierPidgeon/BatchDrivenInterfaceManager_Tests.cs
@@ -26,7 +26,7 @@ namespace CarrierPidgeon.Test.CarrierPidgeon
 
         [Fact]
         [Trait("Category", "unit")]
-        public void ExecuteInterface()
+        public void ExecuteInterface_Success()
         {
             using (var manager = new BatchDrivenInterfaceManager())
             {
@@ -45,6 +45,30 @@ namespace CarrierPidgeon.Test.CarrierPidgeon
                 //Assert
                 Thread.Sleep(1000);
                 Assert.True(manager.Interfaces.FirstOrDefault().IsExecuting);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void ExecuteInterface_DoNotExecuteIfNextExecutionTimeGreaterThanCurrentTime()
+        {
+            using (var manager = new BatchDrivenInterfaceManager())
+            {
+                //Assemble
+                var mock = new Mock<IBatchDriven>();
+                mock.SetupGet(p => p.NextExecutionTime)
+                    .Returns(DateTime.Now.AddSeconds(10000));
+                mock.Setup(i => i.Execute())
+                    .Callback(() => mock.SetupGet(p => p.IsExecuting).Returns(true));
+
+                manager.Add(mock.Object);
+
+                //Act
+                manager.Start();
+
+                //Assert
+                Thread.Sleep(1000);
+                Assert.False(manager.Interfaces.FirstOrDefault().IsExecuting);
             }
         }
     }
