0c1cb3ab27b2e7d67bef8d7de72c3eeb46e1f0aa
Dillon Adams <dillon-a@hotmail.com>
2018-06-19 19:33:22 -0500

    broken checkpoint. wife needs windows box.

--- CarrierPidgeon.Test/CarrierPidgeon/BatchDrivenInterfaceManager_Tests.cs
+++ CarrierPidgeon.Test/CarrierPidgeon/BatchDrivenInterfaceManager_Tests.cs
@@ -1,5 +1,6 @@
-﻿using Moq;
-using System;
+﻿using CarrierPidgeon.Core;
+using Moq;
+using System.Linq;
 using Xunit;
 
 namespace CarrierPidgeon.Test.CarrierPidgeon
@@ -8,6 +9,36 @@ namespace CarrierPidgeon.Test.CarrierPidgeon
     {
         [Fact]
         [Trait("Category", "unit")]
-        public void 
+        public void AddInterface()
+        {
+            //Assemble
+            var bdi = new Mock<IBatchDriven>().Object;
+            var manager = new BatchDrivenInterfaceManager();
+
+            //Act
+            manager.Add(bdi);
+
+            //Assert
+            Assert.Single(manager.Interfaces);
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void ExecuteInterface()
+        {
+            //Assemble
+            var mock = new Mock<IBatchDriven>();
+            mock.Setup(i => i.Execute())
+                .Callback(() => mock.SetupGet(p => p.IsExecuting).Returns(true));
+
+            var manager = new BatchDrivenInterfaceManager();
+            manager.Add(mock.Object);
+
+            //Act
+            manager.Start();
+
+            //Assert
+            Assert.True(manager.Interfaces.FirstOrDefault().IsExecuting);
+        }
     }
 }
