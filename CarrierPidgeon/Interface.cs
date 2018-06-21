352e3ac1a381675e8073e6f6672c616c62b804b3
Dillon Adams <dillon-a@hotmail.com>
2018-06-20 21:32:39 -0500

    adding interface class to manage interface plugins

---
+++ CarrierPidgeon/Interface.cs
@@ -0,0 +1,27 @@
+﻿using CarrierPidgeon.Core;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CarrierPidgeon
+{
+    public class Interface
+    {
+        public Type Type { get; }
+
+        public object Instance => 
+            Activator.CreateInstance(Type);
+         
+
+        public Interface(string path)
+        {
+            var assemblyName = AssemblyName.GetAssemblyName(path);
+            var assembly = Assembly.Load(assemblyName);
+
+            Type = assembly.ExportedTypes.FirstOrDefault(t => !t.IsAbstract && 
+                !t.IsInterface && 
+                (t.IsAssignableFrom(typeof(IBatchDriven)) || t.IsAssignableFrom(typeof(IEventDriven))));
+        }
+    }
+}
