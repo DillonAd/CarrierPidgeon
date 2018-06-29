77d91f9efa85db3b4d1946e8f315045168a5ded6
Dillon <dillon-a@hotmail.com>
2018-06-28 19:35:30 -0500

    adding to batch driven interfaces in core project

--- CarrierPidgeon.Core/BatchDriven/IBatchDrivenReceiver.cs
+++ CarrierPidgeon.Core/BatchDriven/IBatchDrivenReceiver.cs
@@ -1,7 +1,14 @@
-﻿namespace CarrierPidgeon.Core.BatchDriven
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace CarrierPidgeon.Core.BatchDriven
 {
     public interface IBatchDrivenReceiver : IReceiver
     {
-        
+        string Command { get; }
+
+        DataTable Pull(params object[] parameters);
+
+        Task<DataTable> PullAsync(params object[] parameters);
     }
 }
--- CarrierPidgeon.Core/BatchDriven/IBatchDrivenSender.cs
+++ CarrierPidgeon.Core/BatchDriven/IBatchDrivenSender.cs
@@ -1,6 +1,12 @@
-﻿namespace CarrierPidgeon.Core.BatchDriven
+﻿using System.Threading.Tasks;
+
+namespace CarrierPidgeon.Core.BatchDriven
 {
     public interface IBatchDrivenSender : ISender
     {
+        string Command { get; }
+
+        void Push(params object[] parameters);
+        Task PushAsync(params object[] parameters);
     }
 }
