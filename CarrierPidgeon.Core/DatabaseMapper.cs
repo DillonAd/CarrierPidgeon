83a85f0e088fcd30463b659b70245d8f98c7179a
Dillon <dillon-a@hotmail.com>
2018-07-15 12:05:38 -0500

    adding database mapper class

---
+++ CarrierPidgeon.Core/DatabaseMapper.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace CarrierPidgeon.Core
+{
+    public abstract class DatabaseMapper<TEntity>
+        where TEntity : new()
+    {
+        private List<string> _fields;
+        private IEnumerable<string> _properties;
+
+        public DatabaseMapper()
+        {
+            _properties = typeof(TEntity).GetProperties().Select(e => e.Name);
+        }
+
+
+        protected virtual IEnumerable<TEntity> Map(DataTable dataTable)
+        {
+            DataRow row;
+            object value;
+            TEntity entity = new TEntity();
+
+            for(int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                row = dataTable.Rows[i];
+
+                foreach(var property in typeof(TEntity).GetProperties())
+                {
+                    value = row[property.Name];
+                    property.SetValue(entity, value);
+                }
+
+                yield return entity;
+            }
+        }
+    }
+}
